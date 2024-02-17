0d609ed55b739730024a5b5c0a2903b7864da541
Mahimur Rahman Khan <88079080+mahimrahman@users.noreply.github.com>
2024-02-16 21:10:37 -0500

    Update Startup.java

--- src/main/java/de/storchp/opentracks/osmplugin/Startup.java
+++ src/main/java/de/storchp/opentracks/osmplugin/Startup.java
@@ -57,7 +57,7 @@ public class Startup extends Application {
                 @SuppressLint("DiscouragedPrivateApi")
                 var getProcessName = activityThread.getDeclaredMethod("currentProcessName");
                 processName = (String) getProcessName.invoke(null);
-            } catch (Exception e){
+            } catch (Exception ignored){
                 Log.e(TAG, "Error getting process name via reflection", e);} 
         } else {
             processName = Application.getProcessName();
